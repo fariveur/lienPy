bf601085a0bf62bfbaeef22426a7a84a82052988
fariveur <fr7h6hnvjp@privaterelay.appleid.com>
2026-09-19 20:23:56 +0200

    Add files via upload

--- ipsearch.py
+++ ipsearch.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 import os
 
 app = Flask(__name__)
 
 @app.route('/com')
 def executer_script():
-    os.system("shutdown /s /t 0")
+    ip = request.remote_addr
+
+    print("IP du client :", ip)
     print("Le script s'est exécuté !")
+
     return "Script exécuté avec succès !"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
