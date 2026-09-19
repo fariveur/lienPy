953c2b563c739859fb3eb37d1d90f6de37803cf4
fariveur <fr7h6hnvjp@privaterelay.appleid.com>
2026-09-19 20:53:18 +0200

    Add files via upload

--- ipsearch.py
+++ ipsearch.py
@@ -1,13 +1,20 @@
 from flask import Flask, request
-import os
+import requests
 
 app = Flask(__name__)
+
+WEBHOOK_URL = "https://discord.com/api/webhooks/1550941768952844418/wMfHfY4Q3-OStrMILnaB1XkRjURjERSVoDlP4pKLHooJ8Tk8mSRUmwL-bnW3gIyzCbc0"
 
 @app.route('/com')
 def executer_script():
     ip = request.remote_addr
 
     print("IP du client :", ip)
+
+    requests.post(WEBHOOK_URL, json={
+        "content": f"Nouvelle IP : {ip}"
+    })
+
     print("Le script s'est exécuté !")
 
     return "Script exécuté avec succès !"
